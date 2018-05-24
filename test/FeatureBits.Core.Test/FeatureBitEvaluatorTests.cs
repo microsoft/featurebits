f4273d519df35e28e10868beab6d266f326c6342
Yerko Nauls <yenauls@microsoft.com>
2018-05-24 12:31:48 -0400

    added support to pass in a list of feature to be checked.

--- test/FeatureBits.Core.Test/FeatureBitEvaluatorTests.cs
+++ test/FeatureBits.Core.Test/FeatureBitEvaluatorTests.cs
@@ -1,14 +1,11 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using FeatureBits.Data;
 using FluentAssertions;
 using NSubstitute;
-using NSubstitute.Core;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace FeatureBits.Core.Test
@@ -26,7 +23,7 @@ namespace FeatureBits.Core.Test
 
             // Assert
             result.Should().Be(true);
-        }
+        }        
 
         [Fact]
         public async Task It_can_evaluate_a_Simple_FeatureBit_to_false()
@@ -100,13 +97,70 @@ namespace FeatureBits.Core.Test
             result.Should().Be(false);
         }
 
+        [Fact]
+        public async Task It_can_evaluate_a_list_of_Simple_FeatureBits()
+        {
+            // Arrange            
+            var featureBitDefinitions = new List<FeatureBitDefinition>
+            {
+                new FeatureBitDefinition { Id = 1, OnOff = true },
+                new FeatureBitDefinition { Id = 2, OnOff = false },
+                new FeatureBitDefinition { Id = 3, OnOff = true },
+            };
+
+            // Arrange
+            var it = await SetupFeatureBitEvaluator(featureBitDefinitions);
+
+            // Act
+            var flags = it.GetFeatureFlags(new List<int> { 1, 2, 3 });
+
+            // Assert
+            flags[0].Value.Should().Be(true);
+            flags[1].Value.Should().Be(false);
+            flags[2].Value.Should().Be(true);
+        }
+
+        [Fact]
+        public async Task It_can_evaluate_a_list_of_Role_FeatureBits_to_false()
+        {
+            // Arrange
+            // MinimumAllowedPermissionLevel trumps "OnOff"
+            var featureBitDefinitions = new List<FeatureBitDefinition>
+            {
+                new FeatureBitDefinition { Id = 1, OnOff = true, MinimumAllowedPermissionLevel = 20 },
+                new FeatureBitDefinition { Id = 2, OnOff = true, MinimumAllowedPermissionLevel = 30 },
+                new FeatureBitDefinition { Id = 3, OnOff = true, MinimumAllowedPermissionLevel = 40 },
+            };
+
+            // Arrange
+            var it = await SetupFeatureBitEvaluator(featureBitDefinitions);
+
+            // Act
+            var flags = it.GetFeatureFlags(new List<int> { 1, 2, 3 }, 10);
+
+            // Assert
+            flags[0].Value.Should().Be(false);
+            flags[1].Value.Should().Be(false);
+            flags[2].Value.Should().Be(false);
+        }
+
         private static async Task<FeatureBitEvaluator> SetupFeatureBitEvaluator(FeatureBitDefinition bitDefinition)
+        {           
+            return await SetupFeatureBitEvaluator(new List<FeatureBitDefinition> { bitDefinition });
+        }
+
+        private static async Task<FeatureBitEvaluator> SetupFeatureBitEvaluator(IList<FeatureBitDefinition> bitDefinitions)
         {
             var response = new List<FeatureBitDefinition>();
-            if (bitDefinition != null)
+
+            foreach (var bitDefinition in bitDefinitions)
             {
-                response.Add(bitDefinition);
+                if (bitDefinition != null)
+                {
+                    response.Add(bitDefinition);
+                }
             }
+
             var repo = Substitute.For<IFeatureBitsRepo>();
             repo.GetAllAsync().Returns(Task.FromResult((IEnumerable<FeatureBitDefinition>) response));
             var it = await FeatureBitEvaluator.BuildEvaluatorAsync(repo);
