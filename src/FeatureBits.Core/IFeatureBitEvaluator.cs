f1cd42a3fcae8a449d1541752b17d1d2c88089a9
Yerko Nauls <yenauls@microsoft.com>
2018-05-24 14:43:15 -0400

    added new methods to interface

--- src/FeatureBits.Core/IFeatureBitEvaluator.cs
+++ src/FeatureBits.Core/IFeatureBitEvaluator.cs
@@ -26,6 +26,23 @@ namespace FeatureBits.Core
         bool IsEnabled<T>(T feature, int currentPermissionLevel) where T : struct, IConvertible;
 
         /// <summary>
+        /// Get a list of Features and corresponding state (enabled or disabled)
+        /// </summary>
+        /// <param name="features">list of Features to be checked</param>
+        /// <typeparam name="T">An enumeration or an integer</typeparam>
+        /// <returns>Returns a list of <see cref="KeyValuePair{TKey, bool}"/> mapping Feature to State (disabled or enabled)</returns>
+        IList<KeyValuePair<T, bool>> GetFeatureFlags<T>(IList<T> features) where T : struct, IConvertible;
+
+        /// <summary>
+        /// Get a list of Features and corresponding state (enabled or disabled)
+        /// </summary>
+        /// <param name="features">list of Features to be checked</param>
+        /// <param name="currentPermissionLevel">The permission level of the current user</param>
+        /// <typeparam name="T">An enumeration or an integer</typeparam>
+        /// <returns>Returns a list of <see cref="KeyValuePair{TKey, bool}"/> mapping Feature to State (disabled or enabled)</returns>
+        IList<KeyValuePair<T, bool>> GetFeatureFlags<T>(IList<T> features, int currentPermissionLevel) where T : struct, IConvertible;
+
+        /// <summary>
         /// Gets a copy of the list of feature bits from the data store
         /// </summary>
         IList<FeatureBitDefinition> Definitions { get; }
