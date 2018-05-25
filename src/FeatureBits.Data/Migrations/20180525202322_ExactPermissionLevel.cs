3480e53de2514dbf5c09c4bcafa7129c30df4a77
Mitch Klann <mklann@microsoft.com>
2018-05-25 15:23:57 -0500

    Adding ExactPermissionLevel EF Migration

---
+++ src/FeatureBits.Data/Migrations/20180525202322_ExactPermissionLevel.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace FeatureBitsData.Migrations
+{
+    public partial class ExactPermissionLevel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ExactAllowedPermissionLevel",
+                table: "FeatureBitDefinitions",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ExactAllowedPermissionLevel",
+                table: "FeatureBitDefinitions");
+        }
+    }
+}
